ad8a0b52f807d9da45d332448df8fa9cc502f538
4EKUSHKAq <113303180+4EKUSHKAq@users.noreply.github.com>
2025-10-10 12:22:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", async (req, res) => {
   const userId = "403395020";             // твой id ВК
-  const token = "vk1.a.bRXs0neVIzoJ_EK1aSJrpUtT15AM1H3oMdS1RPVc0uKt6y23n3BEXI0ghmEf0fIr5zUqN9Mb1-vyqI8XIbhnUNKM9hZWovHnFaQlg_9R_IbbWBPfUf4R8e9oxL1aFHfCKon2y4m3YjDNE8JPf-4tK94kITzspt3U1IKqVr5Ot8SC84_zEug9qAUYHdvOj3Mdt7pqUUrMRHCaTlfj279oDQ";              // ← вставь сюда токен
+  const token = "vk1.a.kfAnnzMNcESHiWNtcsn5vOlrZ92BVzPUAq_kH9JYlQF4DKEZxFYqPbNdrYhElw8OQEbRSdkfe3EfuxYq3lbeeyeFpLd56wd0-88TYvF2BP2tFqCJKsuKQM7LVfpytd1zv84LR6xMIZqCLvN4HNncxXy8IbgWCRNU8okKIAbGdJqQRCcWB2b-0jJy7VNwdn1LGPHKAkB4pSOzHdJAcE4mpg";              // ← вставь сюда токен
 
   const api = `https://api.vk.com/method/audio.getBroadcast?owner_id=${userId}&access_token=${token}&v=5.131`;
 
@@ -30,3 +30,4 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`✅ Порт ${PORT}`));
+
