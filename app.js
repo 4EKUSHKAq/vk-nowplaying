02a8a60dd3cdbec66549c7d5b36cad8c3f590c27
4EKUSHKA <113303180+4EKUSHKAq@users.noreply.github.com>
2026-04-16 17:48:18 +0500

    Initialize Express server with basic routes

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { getNowPlaying } from "./vk.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  const text = await getNowPlaying();
+  res.send(text);
+});
+
+app.get("/ping", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(PORT, () => {
+  console.log("Server started");
+});
